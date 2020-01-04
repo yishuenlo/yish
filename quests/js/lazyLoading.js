d2a5d4daf0d805625c647f0327a59542d3fdc6bb
Yishuen Lo <lo.yishuen@gmail.com>
2020-01-03 20:56:12 -0700

    daily log

--- quests/js/lazyLoading.js
+++ quests/js/lazyLoading.js
@@ -1,12 +1,30 @@
-var lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
-let lazyimg = document.querySelectorAll('img.lazy');
-
-lazyImages[0].setAttribute('alt', 'hello cat!');
-lazyimg[0].setAttribute('alt', 'hello cat!');
-
 document.addEventListener("DOMContentLoaded", function(){
 
     //create an array, and store NodeList
     var lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+
+    //do this only if intersectionObserver is supported
+    if("IntersectionObserver" in window){
+
+        //create new observer object
+        let LazyImageObserver = new IntersectionObserver(function(entries, observer){
+            entries.forEach(function(entry){
+                //do these if the target intersects with the root
+                if(entry.isIntersecting){
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.classList.remove('lazy');
+
+                    //stop observing this image
+                    LazyImageObserver.unobserve(lazyImage);
+                }
+            })
+        });
+
+        //loop through and observe each image
+        lazyImages.forEach(function(lazyImages){
+            LazyImageObserver.observe(lazyImages);
+        })
+    }
 
 })
