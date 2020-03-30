f8c8a3775363c4d0c6c1e9fd16fd7fe465a94961
Yishuen Lo <lo.yishuen@gmail.com>
2020-03-29 19:10:02 -0600

    gamification stats UI

--- js/script.js
+++ js/script.js
@@ -20,10 +20,7 @@ for (i = 0; i < ANCHOR_TAGS.length; i++) {
     }
 }
 
-
-//DOM - log components
-const LEVEL_DISPLAY = document.querySelectorAll('.level-display');
-const CURRENT_EXP = document.querySelectorAll('.current-exp');
+//nav bar
 const NAV_BAR = document.querySelector('.nav-ul');
 
 NAV_BAR.innerHTML = `
@@ -32,19 +29,9 @@ NAV_BAR.innerHTML = `
     <li><a href="about.html">about</a></li>
 `;
 
-
-
-
-
-// experience points 
-const TRAIN_CONSUME_EXP = 0.50;
-const TRAIN_CREATE_EXP = 1.00;
-const QUEST_TUTORIAL_EXP = 5;
-const QUEST_MINIJS_EXP = 2; 
-
 //exp calculator 
-function level(level){
-    return Math.floor((15 * Math.pow(level,3)) / 5);
+function levelExp(level) {
+    return Math.floor((15 * Math.pow(level, 3)) / 5);
 }
 
 // DOM - level table
@@ -52,37 +39,70 @@ const PRINT_LEVEL = document.querySelectorAll('.level-table');
 const PRINT_EXP = document.querySelectorAll('.exp-table');
 const PRINT_EXP_TNL = document.querySelectorAll('.exp-tnl');
 
-//current level display based on experience points_EXP
-function currentLevel(exp){
-    if (exp <= level(1)) {
-        console.log('level 1');
-    } else if (exp <= level(2)) {
-        LEVEL_DISPLAY.innerHTML = 'level 2';
-    } else if(exp <= level(3)) {
-        console.log('level 3');
-    } else if(exp <= level(4)){
-        console.log('level 4');
-    } else if (exp <= level(5)) {
-        console.log('level 5');
-    } else if (exp <= level(6)) {
-        console.log('level 6');
-    } else if (exp <= level(7)) {
-        console.log('level 7');
-    } else if (exp <= level(8)) {
-        console.log('level 8');
-    } else if (exp <= level(9)) {
-        console.log('level 9');
-    } else {
-        console.log('level 10');
-    }
-};
-
 //populate level & experience table
 for (i = 0; i < PRINT_LEVEL.length; i++) {
     PRINT_LEVEL[i].innerHTML = `level ${i + 1}`;
-    PRINT_EXP[i].innerHTML = `$${level(i + 1)} EXP`;
-    PRINT_EXP_TNL[i].innerHTML = `$${level(i + 2) - level(i + 1)} EXP`;
+    PRINT_EXP[i].innerHTML = `$${levelExp(i + 1)} EXP`;
+    PRINT_EXP_TNL[i].innerHTML = `$${levelExp(i + 2) - levelExp(i + 1)} EXP`;
 }
+
+//DOM - log components
+const CURRENT_EXP_DISPLAY = document.querySelector('.currentEXP');
+const CURRENT_LEVEL_EXP_DISPLAY = document.querySelector('.currentLevelEXP');
+let CURRENT_LEVEL_BADGE = document.querySelector('.current-level');
+let currentLevel = 0;
+
+//get current exp from article entry
+let currentExp = document.querySelector('.current-exp').innerHTML;
+//convert to number
+currentExp = parseInt(currentExp.slice(1, currentExp.length));
+
+let currentProgress = document.querySelector('.current-progress');
+
+
+//current level display based on experience points_EXP
+function calcCurrentLevel(exp){
+ 
+    if (exp <= levelExp(2)) {
+        currentLevel = 1;
+    } else if (exp <= levelExp(3)) {
+        currentLevel = 2;
+    } else if(exp <= levelExp(4)) {
+        currentLevel = 3;
+    } else if(exp <= levelExp(5)){
+        currentLevel = 4;
+    } else if (exp <= levelExp(6)) {
+        currentLevel = 5;
+    } else if (exp <= levelExp(7)) {
+        currentLevel = 6;
+    } else if (exp <= levelExp(8)) {
+        currentLevel = 7;
+    } else if (exp <= levelExp(9)) {
+        currentLevel = 8;
+    } else if (exp <= levelExp(10)) {
+        currentLevel = 9;
+    } else {
+        currentLevel = 10;
+    }
+
+    return currentLevel;
+};
+
+//display current level based on current exp
+CURRENT_LEVEL_BADGE.innerHTML = calcCurrentLevel(currentExp);
+
+//show progress length based on current exp
+currentProgress.style.width = `${currentExp / levelExp(currentLevel+1)*100}%`;
+
+//show progress text
+CURRENT_EXP_DISPLAY.innerHTML = currentExp;
+CURRENT_LEVEL_EXP_DISPLAY.innerHTML = levelExp(currentLevel + 1) - 1;
+
+// experience points 
+const TRAIN_CONSUME_EXP = 0.50;
+const TRAIN_CREATE_EXP = 1.00;
+const QUEST_TUTORIAL_EXP = 5;
+const QUEST_MINIJS_EXP = 2;
 
 
 
